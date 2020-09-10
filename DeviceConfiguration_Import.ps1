03687c4b52102d1956dc457335c76ae1e262dd70
matthewpottinger <67735557+matthewpottinger@users.noreply.github.com>
2020-09-10 12:56:55 +0100

    Update DeviceConfiguration_Import.ps1

--- DeviceConfiguration_Import.ps1
+++ DeviceConfiguration_Import.ps1
@@ -584,7 +584,7 @@ $AADGroupConfigure = 'Do you want to configure Group Assignment [Y/N]'
     }
     }
     else {
-        write-host "Device Configuration policie will be created with no permissions" -ForegroundColor red
+        write-host "Device Configuration policy will be created with no assignments" -ForegroundColor red
     }
 
 
